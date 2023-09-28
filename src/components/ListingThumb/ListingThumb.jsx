2549c0e0e11d2c9dfe29e7ac84d3342ac524c7a0
Adrien <adrien.galibert@gmail.com>
2023-09-28 17:35:27 +0200

    Add hover transition on listingThumb

--- src/components/ListingThumb/ListingThumb.jsx
+++ src/components/ListingThumb/ListingThumb.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./ListingThumb.scss";
 
 function ListingThumb({ data }) {
     const { title, cover } = data
+    const [isHovered, setIsHovered] = useState(false)
 
     return (
         <article className="listing-thumb">
-            <h2 className="listing-thumb__title">{title}</h2>
-            <img src={cover} alt="" className="listing-thumb__cover" />
+            <h2 className="listing-thumb__title"><a
+                                                    href="#"
+                                                    className="listing-thumb__link"
+                                                    onMouseEnter={() => setIsHovered(true)}
+                                                    onMouseLeave={() => setIsHovered(false)}
+                                                    >
+                                                        {title}
+                                                </a>
+            </h2>
+            <img src={cover} alt="" className={`listing-thumb__cover ${isHovered ? 'listing-thumb__cover--hover' : ''}`}/>
         </article>
     )
 }
