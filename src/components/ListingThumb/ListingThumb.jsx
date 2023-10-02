4eab6a84a153ab19e41e900750f3a878bd3a39dd
Adrien <adrien.galibert@gmail.com>
2023-10-02 13:19:07 +0200

    Add link to listing thumbs

--- src/components/ListingThumb/ListingThumb.jsx
+++ src/components/ListingThumb/ListingThumb.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 import "./ListingThumb.scss";
 
 function ListingThumb({ data }) {
@@ -7,14 +8,14 @@ function ListingThumb({ data }) {
 
     return (
         <article className="listing-thumb">
-            <h2 className="listing-thumb__title"><a
-                                                    href="#"
+            <h2 className="listing-thumb__title"><Link
+                                                    to={`listing/${id}`}
                                                     className="listing-thumb__link"
                                                     onMouseEnter={() => setIsHovered(true)}
                                                     onMouseLeave={() => setIsHovered(false)}
                                                     >
                                                         {title}
-                                                </a>
+                                                </Link>
             </h2>
             <img src={cover} alt="" className={`listing-thumb__cover ${isHovered ? 'listing-thumb__cover--hover' : ''}`}/>
         </article>
