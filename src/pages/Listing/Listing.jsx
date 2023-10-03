20ab3cc6adca2a02b2884e604aeed7eb0999da11
Adrien <adrien.galibert@gmail.com>
2023-10-03 12:58:22 +0200

    Handle incorrect listing id routing

--- src/pages/Listing/Listing.jsx
+++ src/pages/Listing/Listing.jsx
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import jsonData from "../../data/logements.json"
 import Slideshow from '../../components/Slideshow'
 import ListingMain from '../../components/ListingMain'
@@ -6,15 +7,30 @@ import ListingDetails from '../../components/ListingDetails'
 
 function Listing() {
     const { listingId } = useParams()
+    const navigate = useNavigate();
+
     
     const findListingById = (id) => {
         return jsonData.find((listing) => listing.id === id)
     }
 
     const listingData = findListingById(listingId);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!listingData) {
+            navigate('/404')
+        } else {
+            setLoading(false)
+        }
+    }, [listingData, navigate])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
     if (!listingData) {
-        return null
+        return <div>Listing not found</div>;
     }
 
     const { title, pictures, description, host, rating, location, equipments, tags } = listingData
