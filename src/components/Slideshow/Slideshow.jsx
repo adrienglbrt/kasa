764c27fab83dda17a4b15b4834d80bdd5cc93d76
Adrien <adrien.galibert@gmail.com>
2023-10-03 13:43:37 +0200

    Add single slide behavior to Slideshow component

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -12,12 +12,20 @@ function Slideshow({ data }) {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length)
     }
 
+    const isSingleSlide = data.length <= 1
+
     return (
         <div className='slideshow'>
-            <button className='slideshow__arrow slideshow__arrow--prev' onClick={prevSlide}><span className='sr-only'>Previous</span></button>
+            {!isSingleSlide && (
+                <button className='slideshow__arrow slideshow__arrow--prev' onClick={prevSlide}><span className='sr-only'>Previous</span></button>
+            )}
             <img src={data[currentIndex]} className='slideshow__current-img' alt={`Slide ${currentIndex}`} />
-            <span className='slideshow__index'>{currentIndex + 1}/{data.length}</span>
-            <button className='slideshow__arrow slideshow__arrow--next' onClick={nextSlide}><span className='sr-only'>Next</span></button>
+            {!isSingleSlide && (
+                <span className='slideshow__index'>{currentIndex + 1}/{data.length}</span>
+            )}
+            {!isSingleSlide && (
+                <button className='slideshow__arrow slideshow__arrow--next' onClick={nextSlide}><span className='sr-only'>Next</span></button>
+            )}
         </div>
     )
 }
