63e5bba553d863dff521bb59c330c3b403d5c38d
Adrien <adrien.galibert@gmail.com>
2023-10-03 13:05:00 +0200

    Add keys to rating stars

--- src/components/ListingRating/ListingRating.jsx
+++ src/components/ListingRating/ListingRating.jsx
@@ -11,13 +11,13 @@ function ListingRating({ data }) {
     const stars = []
     for (let i = 0; i < filledStars; i++) {
         stars.push(
-            <img src={filledStar} className='listing-rating__star listing-rating__star--filled' alt='filled rating star'/>
+            <img src={filledStar} className='listing-rating__star listing-rating__star--filled' alt='filled rating star' key={i+1}/>
         )
     }
     if (emptyStars !== 0) {
         for (let i = 0; i < emptyStars; i++) {
             stars.push(
-                <img src={emptyStar} className='listing-rating__star listing-rating__star--empty' alt='empty rating star'/>
+                <img src={emptyStar} className='listing-rating__star listing-rating__star--empty' alt='empty rating star' key={maxStars - (i -1)} />
             )
         }
     }
